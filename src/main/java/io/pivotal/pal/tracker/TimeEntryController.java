e62b411e1e345a6d5ca29d6316c46a1ab1ec2827
Kroger PAL04 <ted.randy.pal04@kroger.com>
2017-10-03 14:27:58 -0400

    fix controller

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/timeEntry")
+@RequestMapping("/time-entries")
 public class TimeEntryController {
 
     private TimeEntryRepository timeEntryRepository;
